f9199834ffbb15db3b463dd0366f4df42ff6df87
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-03-06 12:35:26 +0530

    Update variables.tf

--- vpc-product/modules/vpc/variables.tf
+++ vpc-product/modules/vpc/variables.tf
@@ -1,38 +1,19 @@
-# modules/vpc/variables.tf
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
 
-variable "cidr_block" {
+variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
-  default     = "10.0.0.0/16"
 }
 
-variable "vpc_name" {
-  description = "Name for the VPC"
-  type        = string
-  default     = "MainVPC"
+variable "public_subnets" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
 }
 
-variable "subnet_cidr_block" {
-  description = "CIDR block for the subnet"
-  type        = string
-  default     = "10.0.1.0/24"
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
 }
-
-variable "subnet_name" {
-  description = "Name for the subnet"
-  type        = string
-  default     = "Subnet-1"
-}
-
-variable "availability_zone" {
-  description = "The availability zone to launch the subnet in"
-  type        = string
-  default     = "us-east-1a"
-}
-
-variable "map_public_ip_on_launch" {
-  description = "Whether to assign public IP addresses to instances launched in the subnet"
-  type        = bool
-  default     = true
-}
-
