55326a3a286fc383444c4dfff4d243c6fa194248
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-03-06 12:06:17 +0530

    Update variables.tf

--- ec2-product/modules/ec2/variables.tf
+++ ec2-product/modules/ec2/variables.tf
@@ -12,3 +12,7 @@ variable "instance_name" {
   description = "EC2 instance name"
   type        = string
 }
+variable "subnet_id" {
+  description = "List of subnet IDs where EC2 instances will be deployed"
+  type        = string
+}
