a7a77a3eee1771fff01f53fc127395ea7cf2d990
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-03-06 12:09:31 +0530

    Update outputs.tf

--- vpc-product/modules/vpc/outputs.tf
+++ vpc-product/modules/vpc/outputs.tf
@@ -1,12 +1,10 @@
-# modules/vpc/outputs.tf
-
 output "vpc_id" {
-  description = "The ID of the created VPC"
-  value       = aws_vpc.main.id
+  description = "The ID of the VPC"
+  value       = aws_vpc.scvpc.id
 }
 
-output "subnet_id" {
-  description = "The ID of the created Subnet"
-  value       = aws_subnet.subnet_1.id
+output "public_subnets" {
+  description = "List of public subnet IDs"
+  value       = aws_subnet.public[*].id
 }
 
